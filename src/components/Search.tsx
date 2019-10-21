3103b0aa17ad3b4707ad5c154a7d8f65ed3afa21
Mahmoud Gamie <m.ismail@speero.net>
2019-10-21 16:41:13 +0200

    created search comp and installed rxjs

---
+++ src/components/Search.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div>
+        <label>
+          Search User
+          <input type="text" />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default Search;
