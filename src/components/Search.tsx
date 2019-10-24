15384bb6964b3d8b85ad6d65703f43ffd6caa4e6
Mahmoud Gamie <m.ismail@speero.net>
2019-10-24 15:30:16 +0200

    implemented direct-user input box

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export interface Props {
+export interface ISearchState {
+  username: string;
+}
+
+export interface ISearchProps {
   searchValue: any;
 }
 
-class Search extends React.Component<Props> {
+class Search extends React.Component<ISearchProps, ISearchState> {
+  state = {
+    username: ""
+  };
+
+  setUsername = (e: any) => {
+    this.setState({ username: e.target.value });
+  };
+
   render() {
     return (
       <div>
@@ -12,6 +25,12 @@ class Search extends React.Component<Props> {
           Search User
           <input type="text" onKeyUp={e => this.props.searchValue(e)} />
         </label>
+
+        <label>
+          Type in a user name
+          <input type="text" onKeyUp={e => this.setUsername(e)} />
+        </label>
+        <Link to={`/view-user/${this.state.username}`}>Go to user Details</Link>
       </div>
     );
   }
