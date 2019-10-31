1d4c10f80ec17d46540b7f0eeb8df8dc4eaa54b7
Mahmoud Gamie <m.ismail@speero.net>
2019-10-31 17:49:35 +0200

    changed app title

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -19,35 +19,3 @@ export default interface IUser {
   type: string;
   url: string;
 }
-
-avatar_url: "https://avatars0.githubusercontent.com/u/130193?v=4";
-bio: "Code goes here, but also @hatnote & @simplelegal. @paypal alumnus. Belaboring the finer points of software on sedimental.org. Focused on curating ecosystems atm";
-blog: "twitter.com/mhashemi";
-company: "@SimpleLegal";
-created_at: "2009-09-23T00:54:13Z";
-email: null;
-events_url: "https://api.github.com/users/mahmoud/events{/privacy}";
-followers: 1559;
-followers_url: "https://api.github.com/users/mahmoud/followers";
-following: 169;
-following_url: "https://api.github.com/users/mahmoud/following{/other_user}";
-gists_url: "https://api.github.com/users/mahmoud/gists{/gist_id}";
-gravatar_id: "";
-hireable: null;
-html_url: "https://github.com/mahmoud";
-id: 130193;
-location: "San Jose, CA";
-login: "mahmoud";
-name: "Mahmoud Hashemi";
-node_id: "MDQ6VXNlcjEzMDE5Mw==";
-organizations_url: "https://api.github.com/users/mahmoud/orgs";
-public_gists: 145;
-public_repos: 71;
-received_events_url: "https://api.github.com/users/mahmoud/received_events";
-repos_url: "https://api.github.com/users/mahmoud/repos";
-site_admin: false;
-starred_url: "https://api.github.com/users/mahmoud/starred{/owner}{/repo}";
-subscriptions_url: "https://api.github.com/users/mahmoud/subscriptions";
-type: "User";
-updated_at: "2019-10-10T16:56:14Z";
-url: "https://api.github.com/users/mahmoud";
